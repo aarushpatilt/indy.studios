4a10ea12145467a12e0a1c5f6d3e433f119eb0c4
Aarush Patil <aarushpatil252@gmail.com>
2023-07-18 13:29:15 -0500

    infinite scroll liked

--- lib/FrontEnd/MenuFlow/LikedSongsView.dart
+++ lib/FrontEnd/MenuFlow/LikedSongsView.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../../Backend/FirebaseComponents.dart';
 import '../../Backend/GlobalComponents.dart';
 import '../../FrontEndComponents/TextComponents.dart';
@@ -9,22 +11,15 @@ class LikedSongView extends StatefulWidget {
 }
 
 class _LikedSongViewState extends State<LikedSongView> {
-  Future<List<Map<String, dynamic>>>? songData;
+  late LikedSongsProvider _likedSongsProvider;
   String backgroundImageUrl = '';
 
   @override
   void initState() {
     super.initState();
-    songData = fetchLikedSongsData();
+    _likedSongsProvider = LikedSongsProvider();
+    _likedSongsProvider.fetchLikedSongsData();
     fetchBackgroundImageUrl();
-  }
-
-  Future<List<Map<String, dynamic>>> fetchLikedSongsData() async {
-    final data = await FirebaseComponents().getReferencedData(
-      collectionPath: 'users/${GlobalVariables.userUUID}/liked_songs',
-      limit: 3,
-    );
-    return data;
   }
 
   Future<void> fetchBackgroundImageUrl() async {
@@ -47,93 +42,131 @@ class _LikedSongViewState extends State<LikedSongView> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            expandedHeight: screenHeight * 0.4,
-            backgroundColor: Colors.black,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  if (backgroundImageUrl.isNotEmpty)
-                    Image.network(
-                      backgroundImageUrl,
-                      width: screenWidth,
-                      height: screenHeight * 0.4,
-                      fit: BoxFit.cover,
-                    ),
-                  Container(
-                    color: Colors.black.withOpacity(0.3),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: EdgeInsets.only(top: 50),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: GlobalVariables.horizontalSpacing),
-                height: screenHeight,
-                width: screenWidth,
-                color: Colors.transparent,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const ProfileText600(
-                          text: 'Liked Songs',
-                          size: 35,
+      body: ChangeNotifierProvider<LikedSongsProvider>.value(
+        value: _likedSongsProvider,
+        builder: (context, child) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: screenHeight * 0.4,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      if (backgroundImageUrl.isNotEmpty)
+                        Image.network(
+                          backgroundImageUrl,
+                          width: screenWidth,
+                          height: screenHeight * 0.4,
+                          fit: BoxFit.cover,
                         ),
-                        ProfilePicture(
-                          size: 70,
+                      Container(
+                        color: Colors.black.withOpacity(0.3),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: GlobalVariables.horizontalSpacing),
+                    height: screenHeight,
+                    width: screenWidth,
+                    color: Colors.transparent,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const ProfileText600(
+                              text: 'Liked Songs',
+                              size: 35,
+                            ),
+                            ProfilePicture(
+                              size: 70,
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16), // Add additional spacing if needed
+                        Consumer<LikedSongsProvider>(
+                          builder: (context, provider, _) {
+                            if (provider.isLoading) {
+                              return CircularProgressIndicator();
+                            } else if (provider.hasError) {
+                              return Text('Error: ${provider.error}');
+                            } else {
+                              final songList = provider.songList;
+                              return Column(
+                                children: songList?.map((song) {
+                                  return SongRow(
+                                    imageUrl: song['image_urls'][1],
+                                    songTitle: song['title'],
+                                    songArtist: song['artists'],
+                                    timestamp: song['timestamp'].toDate(),
+                                    audioUrl: song['image_urls'][0],
+                                    albumId: song['album_id'],
+                                    userID: song['user_id'],
+                                    tags: song['tags'],
+                                    barColor: Colors.green,
+                                    uniqueID: song['unique_id'],
+                                  );
+                                }).toList() ?? [],
+                              );
+                            }
+                          },
                         ),
                       ],
                     ),
-                    SizedBox(height: 16), // Add additional spacing if needed
-                    FutureBuilder<List<Map<String, dynamic>>>(
-                      future: songData,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState == ConnectionState.waiting) {
-                          return CircularProgressIndicator();
-                        } else if (snapshot.hasError) {
-                          return Text('Error: ${snapshot.error}');
-                        } else {
-                          final songList = snapshot.data;
-                          return Column(
-                            children: songList?.map((song) {
-                              return SongRow(
-                                imageUrl: song['image_urls'][1],
-                                songTitle: song['title'],
-                                songArtist: song['artists'],
-                                timestamp: song['timestamp'].toDate(),
-                                audioUrl: song['image_urls'][0],
-                                albumId: song['album_id'],
-                                userID: song['user_id'],
-                                tags: song['tags'],
-                                barColor: Colors.green,
-                                uniqueID: song['unique_id'],
-                                
-                              );
-                            }).toList() ?? [],
-                          );
-                        }
-                      },
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ),
-        ],
+            ],
+          );
+        },
       ),
     );
   }
 }
 
+class LikedSongsProvider with ChangeNotifier {
+  final List<Map<String, dynamic>> _songList = [];
+  bool _isLoading = false;
+  bool _hasError = false;
+  String _error = '';
+
+  List<Map<String, dynamic>> get songList => _songList;
+  bool get isLoading => _isLoading;
+  bool get hasError => _hasError;
+  String get error => _error;
+
+  Future<void> fetchLikedSongsData() async {
+    try {
+      _isLoading = true;
+      notifyListeners();
+
+      final data = await FirebaseComponents().getReferencedData(
+        collectionPath: 'users/${GlobalVariables.userUUID}/liked_songs',
+        limit: 6,
+      );
+
+      _songList.addAll(data);
+      _isLoading = false;
+      _hasError = false;
+      _error = '';
+    } catch (error) {
+      _isLoading = false;
+      _hasError = true;
+      _error = error.toString();
+    } finally {
+      notifyListeners();
+    }
+  }
+}
 
 class SongRow extends StatelessWidget {
   final String imageUrl;
@@ -189,51 +222,48 @@ class SongRow extends StatelessWidget {
     );
   }
 
-void _showMusicTile(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    isScrollControlled: true,
-    backgroundColor: Colors.transparent, // makes the background transparent
-    builder: (BuildContext context) {
-      return DraggableScrollableSheet(
-        initialChildSize: 0.9, // initial height is 90% of screen height
-        minChildSize: 0.5, // minimum height is 50% of screen height
-        maxChildSize: 0.9, // maximum height is 90% of screen height
-        builder: (BuildContext context, ScrollController scrollController) {
-          return ClipRect(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
-                color: Colors.black,
-              ),
-              child: ListView(
-                controller: scrollController,
-                children: [
-                  Transform.translate(
-                    offset: Offset(0, -100), // Shifting the MusicTile upwards by 50 pixels
-                    child: MusicTile(
-                      title: songTitle,
-                      artist: songArtist,
-                      timestamp: timestamp,
-                      imageUrl: imageUrl,
-                      audioUrl: audioUrl,
-                      albumId: albumId,
-                      userID: userID,
-                      tags: tags,
-                      barColor: barColor,
-                      uniqueID: uniqueID,
+  void _showMusicTile(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          initialChildSize: 0.9,
+          minChildSize: 0.5,
+          maxChildSize: 0.9,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return ClipRect(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)),
+                  color: Colors.black,
+                ),
+                child: ListView(
+                  controller: scrollController,
+                  children: [
+                    Transform.translate(
+                      offset: Offset(0, -100),
+                      child: MusicTile(
+                        title: songTitle,
+                        artist: songArtist,
+                        timestamp: timestamp,
+                        imageUrl: imageUrl,
+                        audioUrl: audioUrl,
+                        albumId: albumId,
+                        userID: userID,
+                        tags: tags,
+                        barColor: barColor,
+                        uniqueID: uniqueID,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-      );
-    },
-  );
-}
-
-
-
+            );
+          },
+        );
+      },
+    );
+  }
 }
