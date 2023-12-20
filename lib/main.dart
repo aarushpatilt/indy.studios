59196fa4b13d10b092c64fe6706c06a4c06c24b7
aarushpatilt <aarushpatil252@gmail.com>
2023-12-20 17:01:00 -0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.android,
+      options: DefaultFirebaseOptions.ios,
   );
 
 //ProfileCustom(userID: GlobalVariables.userUUID,)
